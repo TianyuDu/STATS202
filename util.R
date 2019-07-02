bddbc1e8a0a531506e959dc710aeef12ce96ae92
MasterDoo <masterdu9951@icloud.com>
2019-07-02 02:00:57 -0700

    fix bug

--- util.R
+++ util.R
@@ -1,16 +1,17 @@
 TrainTestSplit <- function(df, train.ratio=0.8, verbose=FALSE) {
   train.size <- floor(train.ratio * nrow(df))
   test.size <- nrow(df) - train.size
-  if (verbose) {
-    cat("train.size=", train.size, "test.size=", test.size)
-  }
   random.idx <- sample(nrow(df), replace=FALSE)
   train.set <- df[
-    random.idx[1:train.size],
+    head(random.idx, train.size),
   ]
   test.set <- df[
-    random.idx[train.size+1:nrow(df)],
+    tail(random.idx, test.size),
   ]
+  
+  if (verbose) {
+    cat("train.size=", nrow(train.set), "test.size=", nrow(test.set))
+  }
   return(list(
     "train"=train.set,
     "test"=test.set
