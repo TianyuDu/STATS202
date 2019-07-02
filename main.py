00abb425a8db0cdb1b3eea5dbb9ecd5cdf1552ca
MasterDoo <masterdu9951@icloud.com>
2019-07-01 23:58:08 -0700

    update

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import pandas as pd
 import load_data
 
 if __name__ == "__main__":
-    pass
+    # Load the whole dataset
+    df_whole = load_data.load_whole(path="./data/")
+    # Save to csv
+    df_whole.to_csv("./data/train.csv")
