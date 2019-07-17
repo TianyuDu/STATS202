76db7afb0223a6716ec4881ee7cf2da4d1fd74e9
MasterDoo <masterdu9951@icloud.com>
2019-07-17 14:16:30 -0700

    upadte

--- neuralnet_2.py
+++ neuralnet_2.py
@@ -11,13 +11,15 @@ import data
 class NN(tf.keras.Model):
     def __init__(self):
         super(NN, self).__init__()
-        self.d1 = tf.keras.layers.Dense(64, activation="relu")
-        self.d2 = tf.keras.layers.Dense(32, activation="relu")
+        self.d1 = tf.keras.layers.Dense(256, activation="sigmoid")
+        # self.drop1 = tf.keras.layers.Dropout(0.2)
+        self.d2 = tf.keras.layers.Dense(512, activation="sigmoid")
         # Output layer.
-        self.out = tf.keras.layers.Dense(1, activation="linear")
+        self.out = tf.keras.layers.Dense(1, activation="sigmoid")
 
     def call(self, x):
         x = self.d1(x)
+        # X = self.drop1(x)
         x = self.d2(x)
         return self.out(x)
 
@@ -40,8 +42,6 @@ def test_step(x, y):
     loss = loss_object(y, pred)
     test_loss(loss)
     test_accuracy(y, pred)
-    test_auc.update_state(y, pred)
-    print(test_auc.result().numpy())
 
 
 if __name__ == "__main__":
@@ -49,22 +49,42 @@ if __name__ == "__main__":
     df = data.load_whole("./data/")
     X, y = data.gen_sup(df)
     X = X.astype(np.float32)
+    X, y = map(lambda z: z.values, (X, y))
+    y = y.reshape(-1, 1)
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
-        X, y, test_size=0.3, random_state=None, shuffle=True)
+        X, y, test_size=0.2, random_state=None, shuffle=True)
+
+    train_ds = tf.data.Dataset.from_tensor_slices(
+        (X_train, y_train)).shuffle(10000).batch(32)
+
+    test_ds = tf.data.Dataset.from_tensor_slices(
+        (X_test, y_test)).batch(32)
 
     model = NN()
     loss_object = tf.keras.losses.BinaryCrossentropy()
-    optimizer = tf.keras.optimizers.Adam()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
 
-    model = NN()
-    model.compile(optimizer, loss=loss_object, metrics=["accuracy"])
-    model.fit(
-        x=X_train.values,
-        y=y_train.values,
-        batch_size=32,
-        epochs=30,
-        verbose=1,
-        validation_split=0.2
-    )
-    test_loss = model.evaluate(x=X_test, y=y_test)
-    print(f"Test set result: cross-entropy loss {test_loss[0]: 0.6f}, accuracy {test_loss[1]: 0.6f}")
+    train_loss = tf.keras.metrics.Mean(name="train_loss")
+    train_accuracy = tf.keras.metrics.BinaryAccuracy(
+        name="train_accuracy")
+
+    test_loss = tf.keras.metrics.Mean(name="test_loss")
+    test_accuracy = tf.keras.metrics.BinaryAccuracy(
+        name="test_accuracy")
+
+    EPOCHS = 100
+    for epoch in range(EPOCHS):
+        for x, y in train_ds:
+            train_step(x, y)
+
+        for t_x, t_y in test_ds:
+            test_step(t_x, t_y)
+
+        template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+        print(template.format(
+            epoch+1,
+            train_loss.result(),
+            train_accuracy.result()*100,
+            test_loss.result(),
+            test_accuracy.result()*100)
+        )
