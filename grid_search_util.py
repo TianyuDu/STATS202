fcd60cf59347f2a9cb6fbea8792753ea0824e1d3
MasterDoo <masterdu9951@icloud.com>
2019-07-28 20:26:19 -0700

    add grid_search

--- grid_search_util.py
+++ grid_search_util.py
@@ -14,3 +14,23 @@ def profile_generator(scope: Dict[str, List]) -> List[dict]:
     for vals in itertools.product(*list(src.values())):
         # gen.append(dict(zip(src.keys(), vals)))
         yield dict(zip(src.keys(), vals))
+
+
+def grid_search(
+        scope: dict,
+        data_feed: callable,
+        train_main: callable,
+        log_dir: str = "./grid_result.csv",
+) -> None:
+    header_written = False
+    with open(log_dir, "w") as f:
+        for profile in profile_generator(scope):
+            result = train_main(data_feed, *profile)
+            if not header_written:
+                f.write(",".join(result.keys()))
+                f.write("\n")
+                header_written = True
+            f.write(",".join(
+                [str(x).replace(",", ";") for x in result.values()]
+            ))
+            f.write("\n")
