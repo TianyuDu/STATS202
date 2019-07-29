29409ef9341af1e5b52abe323863ff57e3e38b47
MasterDoo <masterdu9951@icloud.com>
2019-07-28 20:28:29 -0700

    update profile_generator()

--- grid_search_util.py
+++ grid_search_util.py
@@ -10,10 +10,12 @@ def profile_generator(scope: Dict[str, List]) -> List[dict]:
     for k, v in src.items():
         if type(v) not in [list, tuple]:
             src[k] = [v]
-    # gen = list()
+    gen = list()
     for vals in itertools.product(*list(src.values())):
-        # gen.append(dict(zip(src.keys(), vals)))
-        yield dict(zip(src.keys(), vals))
+        gen.append(dict(zip(src.keys(), vals)))
+        # yield dict(zip(src.keys(), vals))
+    print(f"Profiles generated: {len(gen)}")
+    return gen
 
 
 def grid_search(
