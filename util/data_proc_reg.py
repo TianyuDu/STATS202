3ade54f6a7120540b8211bd3e78f983a39f5b04c
MasterDoo <masterdu9951@icloud.com>
2019-08-01 12:03:09 -0700

    update convert_to_patient

--- util/data_proc_reg.py
+++ util/data_proc_reg.py
@@ -8,6 +8,7 @@ AssessmentIDs, and for the regression task, trainining instances
 are indexed using PatientIDs.
 """
 import sys
+from typing import Optional, Union
 import pandas as pd
 import numpy as np
 
@@ -37,17 +38,22 @@ def select_patient(
     return selected
 
 
-
 def convert_to_patient(
         df_assessment: pd.DataFrame,
         min_visit: int = None,
-):
+) -> (pd.DataFrame, Optional[pd.DataFrame]):
     """
     Reshape dataset so that each row of the new dataset corresponds to one
     Patient instead of one Assessment.
     Args:
         df_assessment:
             A dataframe with rows indexed by Assessment ID.
+        
+        include_label:
+            A bool indicating wether to generate the label (last visit PANSS total)
+            from the dataset. Use True when applying on the training set, so that the
+            method returns (X, y), use False on the test set, so that only X is returned.
+
         min_visit:
             Only consider patients with more checkpoints than min_visit.
             If the number of observations is less than min_visit for one
@@ -56,12 +62,6 @@ def convert_to_patient(
         df_patient:
             A dataframe with rows indexed by Patient ID.
     """
-    PANSS = [
-        f"P{i}" for i in range(1, 8)
-    ] + [
-        f"N{i}" for i in range(1, 8)
-    ] + [
-        f"G{i}" for i in range(1, 17)]
     if not "PatientID" in df_assessment:
         raise KeyError("df_assessment DataFame must contain a 'PatientID' column.")
     patient_ids = list(set(df_assessment["PatientID"]))
@@ -69,11 +69,23 @@ def convert_to_patient(
     def retrive_info(pid: int):
         return df_assessment[df_assessment["PatientID"] == pid]
 
-    cache = list()
+    X_lst, y_lst = [], []
     for pid in patient_ids:
-        patient_info = retrive_info(pid)
-        cache.append(len(patient_info))
-    return cache
+        patient_X, patient_y = reduce_patient_features(retrive_info(pid), include_label=include_label)
+        if min_visit is None or len(patient_X) >= min_visit:
+            X_lst.append(patient_X)
+            y_lst.append(patient_y)
+        else:
+            print("Patient dropped.")
+
+    print(f"Numer of patients found: {len(X_lst)}")
+    X = pd.concat(X_lst)
+    if include_label:
+        y = pd.concat(y_lst)
+        assert len(X) == len(y)
+        return X, y
+    else:
+        return X
 
     return df_patient
 
