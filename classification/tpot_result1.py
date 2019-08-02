33c899814cb711c692959fb1360ef625cb1ab526
MasterDoo <masterdu9951@icloud.com>
2019-08-02 01:37:51 -0700

    update

--- classification/tpot_result1.py
+++ classification/tpot_result1.py
@@ -8,6 +8,11 @@ from tpot.builtins import StackingEstimator
 from sklearn.preprocessing import FunctionTransformer
 from copy import copy
 
+sys.path.append("../")
+import util.data_proc
+import util.features
+
+
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
 features = tpot_data.drop('target', axis=1).values
@@ -24,5 +29,18 @@ exported_pipeline = make_pipeline(
     GradientBoostingClassifier(learning_rate=0.01, max_depth=2, max_features=0.15000000000000002, min_samples_leaf=8, min_samples_split=17, n_estimators=100, subsample=0.25)
 )
 
-exported_pipeline.fit(training_features, training_target)
-results = exported_pipeline.predict(testing_features)
+df_train = util.data_proc.load_whole(path="../data/")
+print(df_train.shape)
+df_test = pd.read_csv("../data/Study_E.csv", header=0)
+print(df_test.shape)
+# Reduced countries
+major_countries = ["USA", "Russia", "Ukraine", "China", "Japan"]
+df_train = util.features.reduce_countries(df_train, major_countries)
+df_test = util.features.reduce_countries(df_test, major_countries)
+X_train, y_train, FEATURE, PANSS = util.data_proc.gen_slp_assessment(
+    df_train)
+X_test = util.data_proc.parse_test_set(X_train, df_test)
+print("Design_train: {}, Design_test: {}".format(X_train.shape, X_test.shape))
+
+exported_pipeline.fit(X_train, y_train)
+results = exported_pipeline.predict(X_test)
