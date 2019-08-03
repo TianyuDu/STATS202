d44b674a8f07a2fb79da62ac210cc763d497b2d6
MasterDoo <masterdu9951@icloud.com>
2019-08-03 14:16:22 -0700

    integrate grid_search to RF_classification

--- classification/RF_classification.py
+++ classification/RF_classification.py
@@ -5,12 +5,15 @@ import warnings
 from typing import Union
 import sys
 import argparse
+from datetime import datetime
 
 import numpy as np
 import pandas as pd
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
 from sklearn.metrics import log_loss
 
 sys.path.append("../")
@@ -62,6 +65,52 @@ def classify(path: Union[str, None] = None) -> None:
     if path is not None:
         print("Write submission file to {}".format(path))
         utils.generate_submission(pred_test[:, 1], path)
+
+
+def grid_search(path: Union[str, None] = None) -> None:
+    X_train, y_train, X_test = utils.get_data()
+    X_train, X_dev, y_train, y_dev = train_test_split(
+        X, y, test_size=0.3, random_state=0)
+
+    param_scope = {
+        "max_depth": [None, 20, 30, 50, 100, 200, 300, 500],
+        "n_estimators": [100 * x for x in range(1, 20, 2)],
+        "criterion": ["entropy", "gini"],
+    }
+
+    # Chooose score here.
+    score = "neg_log_loss"
+
+    print("# Tuning hyper-parameters for {}\n".format(score))
+
+    model = GridSearchCV(
+        RandomForestClassifier(),
+        param_scope,
+        cv=5,
+        scoring=score,
+        error_score=np.nan,
+        n_jobs=-1,
+        verbose=1
+    )
+    model.fit(X_train, y_train)
+
+    print("Best parameters set found on development set:\n")
+    print(model.best_params_)
+    now = datetime.strftime(datetime.now(), "%Y_%m_%d_%H_%M")
+    with open("./RF_grid_search_{}.txt".format(now), "w") as f:
+        f.write(str(model.best_params_))
+
+    print("Grid scores on development set:\n")
+    means = model.cv_results_["mean_test_score"]
+    stds = model.cv_results_["std_test_score"]
+    for mean, std, params in zip(means, stds, model.cv_results_["params"]):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+    print("\nDetailed classification report:\n")
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    y_true, y_pred = y_dev, model.predict(X_dev)
+    print(metrics.classification_report(y_true, y_pred))
 
 
 if __name__ == "__main__":
