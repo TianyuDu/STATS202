1a0b337a971b4c32af01390acd39d99acd5ab5eb
MasterDoo <masterdu9951@icloud.com>
2019-08-04 19:17:32 -0700

    update

--- forecasting/RF_regression.py
+++ forecasting/RF_regression.py
@@ -50,7 +50,7 @@ def predict(path: Union[str, None] = None) -> None:
     classification prediction to the destination file.
     """
     # Reading the training data.
-    X_train, y_train, X_test = utils.read_from_disk()
+    X_train, y_train, X_test = utils.get_data()
     # **** Extract ndarray ****
     X_train = X_train.values
     y_train = y_train.values.reshape(-1,)
@@ -77,7 +77,7 @@ def predict(path: Union[str, None] = None) -> None:
     ))
     print("Phase 2: fitting model on the entire training set ...")
     # Re-fit using the entire traininig set.
-    X_train, y_train, X_test = utils.read_from_disk()
+    X_train, y_train, X_test = utils.get_data()
     model.fit(X_train, y_train)
     print("Predicting on the test set ...")
     pred_test = model.predict(X_test)
@@ -88,7 +88,7 @@ def predict(path: Union[str, None] = None) -> None:
 
 
 def grid_search(path: Union[str, None] = None) -> None:
-    X_train, y_train, X_test = utils.read_from_disk()
+    X_train, y_train, X_test = utils.get_data()
     # **** Extract ndarray ****
     X_train = X_train.values
     y_train = y_train.values.reshape(-1,)
