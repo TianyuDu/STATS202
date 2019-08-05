be69c55c6938737e13e1baded7bcda37fefeebb0
MasterDoo <masterdu9951@icloud.com>
2019-08-04 23:38:49 -0700

    update param

--- forecasting/SVR_regression.py
+++ forecasting/SVR_regression.py
@@ -33,9 +33,9 @@ PARAMS = {'C': 128, 'gamma': 1e-05, 'kernel': 'rbf'}
 # Scope for poly kernel
 PARAM_SCOPE = {
     "kernel": ["poly"],
-    "gamma": ["auto"] + [10 ** (-x) for x in range(1, 6)],
-    "C": [2 ** x for x in range(1, 6)],
-    "degree": list(range(3, 8))
+    "gamma": ["auto"] + [10 ** (-x) for x in range(1, 5)],
+    "C": [2 ** x for x in range(1, 5)],
+    "degree": list(range(3, 7))
 }
 
 SCORE = "neg_mean_squared_error"
