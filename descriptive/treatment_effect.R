b343a4e95b93c2a01769b9d14bcde992c3516014
MasterDoo <masterdu9951@icloud.com>
2019-08-05 23:25:45 -0700

    update R file

--- descriptive/treatment_effect.R
+++ descriptive/treatment_effect.R
@@ -6,25 +6,25 @@ df <- read.csv("./Study_A_to_E_95.csv", header=TRUE, sep=",")
 POLY <- 2
 
 model <- lm(
-    formula=PANSS_Total ~ poly(VisitDay, POLY)*Treatment + Treatment + as.factor(Study) + as.factor(Country) + as.factor(Country)*Treatment,
+    formula=PANSS_Total ~ poly(VisitDay, POLY)*Treatment + Treatment + as.factor(Study) + as.factor(Country) + as.factor(Study)*Treatment + as.factor(Country)*Treatment,
     data=df
 )
 summary(model)
 
 model <- lm(
-    formula=P_Total ~ poly(VisitDay, POLY)*Treatment + Treatment + as.factor(Study) + as.factor(Country),
+    formula=P_Total ~ poly(VisitDay, POLY)*Treatment + Treatment + as.factor(Study) + as.factor(Country) + as.factor(Study)*Treatment + as.factor(Country)*Treatment,
     data=df
 )
 summary(model)
 
 model <- lm(
-    formula=N_Total ~ poly(VisitDay, POLY)*Treatment + Treatment + as.factor(Study) + as.factor(Country),
+    formula=N_Total ~ poly(VisitDay, POLY)*Treatment + Treatment + as.factor(Study) + as.factor(Country) + as.factor(Study)*Treatment + as.factor(Country)*Treatment,
     data=df
 )
 summary(model)
 
 model <- lm(
-    formula=G_Total ~ poly(VisitDay, POLY)*Treatment + Treatment + as.factor(Study) + as.factor(Country),
+    formula=G_Total ~ poly(VisitDay, POLY)*Treatment + Treatment + as.factor(Study) + as.factor(Country) + as.factor(Study)*Treatment + as.factor(Country)*Treatment,
     data=df
 )
 summary(model)
