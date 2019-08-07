31036be77c1e1dab443dae7665d70d10668621c9
MasterDoo <masterdu9951@icloud.com>
2019-08-07 16:04:31 -0700

    add regression report

--- forecasting/regression_report.py
+++ forecasting/regression_report.py
@@ -15,7 +15,7 @@ import forecasting.forecasting_utility as utils
 from forecasting.best_models import best_models
 
 
-def model_cv_test(model, X, y, pred_fn: str, n_fold: int = 5):
+def model_cv_test(model, X, y, n_fold: int = 5):
     kf = model_selection.KFold(n_splits=n_fold, shuffle=True)
     cv_loss = []
     for train_index, test_index in kf.split(X):
@@ -24,18 +24,13 @@ def model_cv_test(model, X, y, pred_fn: str, n_fold: int = 5):
         # Fit the model
         model.fit(X_train, y_train)
         # Test the model
-        if pred_fn == "predict":
-            pred_test = model.predict(X_test)
-        elif pred_fn == "predict_proba":
-            pred_test = model.predict_proba(X_test)
-        else:
-            raise ValueError("Invalid predict_fn.")
+        pred_test = model.predict(X_test)
         # Some model predicts both prob y=0 and prob y=1.
         # Extract prob y=1 only.
         if pred_test.shape[1] == 2:
             pred_test = pred_test[:, 1]
-        loss = metrics.log_loss(y_test, pred_test)
-        print("Log loss: {}".format(loss))
+        loss = metrics.mean_squared_error(y_test, pred_test)
+        print("mse loss: {}".format(loss))
         cv_loss.append(loss)
     assert len(cv_loss) == n_fold
     return cv_loss
@@ -62,32 +57,13 @@ if __name__ == "__main__":
     X_train, y_train, X_test = utils.get_data()
     start_time_overall = datetime.now()
     record = list()
-    # Test baseline models
-    for (model, name, params, pred_fn) in best_models:
+    for (model, name, params) in best_models:
         print("Running CV for: {}".format(name))
         start_time = datetime.now()
         constructed_model = model(**params)
         cv_loss = model_cv_test(
             constructed_model,
             X_train.values, y_train.values,
-            pred_fn=pred_fn,
-            n_fold=5
-        )
-        record.append(record_cv_loss(name, cv_loss))
-        end_time = datetime.now()
-        print("Time taken: {}".format(str(end_time - start_time)))
-    # Test Calibrated models
-    for (base_model, name, params, pred_fn) in best_models:
-        name = name + "_cali"
-        print("Running CV for: {}".format(name))
-        start_time = datetime.now()
-        constructed_model = calibration.CalibratedClassifierCV(
-            base_model(**params)
-        )
-        cv_loss = model_cv_test(
-            constructed_model,
-            X_train.values, y_train.values,
-            pred_fn=pred_fn,
             n_fold=5
         )
         record.append(record_cv_loss(name, cv_loss))
