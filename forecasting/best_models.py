f2664061ed37c55e09306095a1dd06b654c31b9f
MasterDoo <masterdu9951@icloud.com>
2019-08-07 20:54:39 -0700

    add doc string

--- forecasting/best_models.py
+++ forecasting/best_models.py
@@ -1,3 +1,6 @@
+"""
+This file stores the best model selected from grid search.
+"""
 from sklearn import ensemble
 from sklearn import svm
 
@@ -5,23 +8,23 @@ best_models = [
     (
         ensemble.RandomForestRegressor,
         "RF",
-        {'criterion': 'mse', 'max_depth': 4096,
-            'max_features': 'auto', 'n_estimators': 500}
+        {"criterion": "mse", "max_depth": 4096,
+            "max_features": "auto", "n_estimators": 500}
     ),
     (
         svm.SVR,
         "SVR_POLY",
-        {'degree': 3, 'kernel': 'poly', 'C': 4, 'gamma': 0.0001}
+        {"degree": 3, "kernel": "poly", "C": 4, "gamma": 0.0001}
     ),
     (
         svm.SVR,
         "SVR_RBF",
-        {'C': 128, 'gamma': 1e-05, 'kernel': 'rbf'}
+        {"C": 128, "gamma": 1e-05, "kernel": "rbf"}
     ),
     (
         ensemble.GradientBoostingRegressor,
         "GB",
-        {'criterion': 'mse', 'max_depth': 4096,
-            'max_features': 'auto', 'n_estimators': 500}
+        {"criterion": "mse", "max_depth": 4096,
+            "max_features": "auto", "n_estimators": 500}
     )
 ]
