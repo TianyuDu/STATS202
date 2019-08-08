c5f160a59c7a12b6bc102b354e420d388d388e54
MasterDoo <masterdu9951@icloud.com>
2019-08-07 20:54:22 -0700

    add doc string

--- classification/best_models.py
+++ classification/best_models.py
@@ -1,3 +1,6 @@
+"""
+This file stores the best model selected from grid search.
+"""
 from sklearn import ensemble
 from sklearn import svm
 from sklearn import linear_model
@@ -6,30 +9,30 @@ best_models = [
     (
         linear_model.LogisticRegression,
         "Logisitc",
-        {'solver': 'saga', 'C': 0.00390625,
-            'l1_ratio': 0.98, 'penalty': 'elasticnet'},
+        {"solver": "saga", "C": 0.00390625,
+            "l1_ratio": 0.98, "penalty": "elasticnet"},
         "predict_proba"
     ),
     (  # RF: best
         ensemble.RandomForestClassifier,
         "RF",
-        {'max_features': 'log2', 'criterion': 'gini',
-            'n_estimators': 1900, 'max_depth': 64,
+        {"max_features": "log2", "criterion": "gini",
+            "n_estimators": 1900, "max_depth": 64,
             "n_jobs": -1},
         "predict_proba"
     ),
     (  # GB
         ensemble.GradientBoostingClassifier,
         "GB",
-        {'max_features': 'sqrt', 'n_estimators': 700,
-            'criterion': 'friedman_mse', 'max_depth': 6,
-            'learning_rate': 0.003},
+        {"max_features": "sqrt", "n_estimators": 700,
+            "criterion": "friedman_mse", "max_depth": 6,
+            "learning_rate": 0.003},
         "predict_proba"
     ),
     (  # SVC
         svm.SVC,
         "SVC",
-        {'kernel': 'rbf', 'gamma': 0.0001, 'probability': True, 'C': 512},
+        {"kernel": "rbf", "gamma": 0.0001, "probability": True, "C": 512},
         "predict_proba"
     )
 ]
