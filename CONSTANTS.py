6f4b7299279c1486495c4933c3b107437c01fca8
MasterDoo <masterdu9951@icloud.com>
2019-08-09 01:08:30 -0700

    update format

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,6 +1,1 @@
-PANSS = [
-        "P"+str(i) for i in range(1, 8)
-    ] + [
-        "N"+str(i) for i in range(1, 8)
-    ] + [
-        "G"+str(i) for i in range(1, 17)]
+PANSS = ["P"+str(i) for i in range(1, 8)] + ["N"+str(i) for i in range(1, 8)] + ["G"+str(i) for i in range(1, 17)]
